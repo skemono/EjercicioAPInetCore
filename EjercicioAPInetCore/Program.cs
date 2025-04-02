461d51e069fcf35bab5c6f9a259029c043fd27ca
skemono <her231038@uvg.edu.gt>
2025-04-01 19:21:15 -0600

    Feat: Swagger implementado y conexion con db sql server.

--- EjercicioAPInetCore/Program.cs
+++ EjercicioAPInetCore/Program.cs
@@ -1,21 +1,26 @@
 using Microsoft.EntityFrameworkCore;
-using TodoApi.Models;
 using IncidentApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
-builder.Services.AddDbContext<TodoContext>(opt =>
-    opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddDbContext<IncidentContext>(opt =>
-    opt.UseInMemoryDatabase("IncidentDB"));
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+// Agregar swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
 }
 
 app.UseHttpsRedirection();
